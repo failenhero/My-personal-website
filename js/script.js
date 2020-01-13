6a92b1e4a410ae1c32617c903672075261307bb2
failenhero <yury_okhrimenko@outlook.com>
2020-01-13 17:51:14 +0300

    popup was added
    popup module and button for it were added but not customized

--- js/script.js
+++ js/script.js
@@ -19,4 +19,26 @@ $(document).ready(function () {
     $('.upbtn').click( () => { 
         document.documentElement.scrollTop = 0;  
     });    
+
+    // Popup
+
+    // open by clicking the button
+    $('.popupButton').click( () => { 
+      $('main').css('filter','blur(5px)');
+      $("#myModalBox").css('display', 'block');
+      $("html,body").css("overflow","hidden");
+    });
+    //closing by close-btn
+    $('.close').click( () => { 
+        $('main').css('filter','none');
+        $("#myModalBox").css('display', 'none');
+        $("html,body").css("overflow","auto");      
+    });
+    //closing by secondary-btn
+    $('.btn-secondary').click( () => { 
+        $('main').css('filter','none');
+        $("#myModalBox").css('display', 'none');
+        $("html,body").css("overflow","auto");      
+    });
+
 });
